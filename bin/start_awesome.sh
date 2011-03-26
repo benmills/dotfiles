913b89b8dd8aef41ff97a637f21d687f72d32f61
= <benmills@bm-air.(none)>
2011-03-26 17:06:03 -0500

    stuff for linux

---
+++ bin/start_awesome.sh
@@ -0,0 +1,5 @@
+gnome-settings-daemon & # handles themes, starts gnome-screensaver. You may have to use gconf to disable it setting the background.
+nm-applet &             # assuming you're using Network Manager
+gnome-power-manager &   # for laptops and stuff
+gnome-volume-manager &  # for mounting CDs, USB sticks, and such
+exec awesome            # awesome receives xinit's process id; when it terminates, X will be terminated
